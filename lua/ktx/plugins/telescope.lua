c568a16f4cca8d87351d9d15bc57b4d992b7868a
James Coulson <git@coulson.ie>
2026-08-29 11:09:29 +0100

    Fix telescope-fzf-native: never built or loaded; add missing pickers
    No build = "make" on the dependency meant lazy never compiled the
    native sorter at all (confirmed live - no .so ever produced), and
    nothing called load_extension("fzf") to actually switch to it -
    installed but completely inert. Also adds buffers/help_tags/oldfiles
    pickers, some of the most commonly used and previously missing
    entirely.

--- lua/ktx/plugins/telescope.lua
+++ lua/ktx/plugins/telescope.lua
@@ -1,9 +1,13 @@
 return {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
+    tag = "v0.1.9", -- was 0.1.5, over 4 point releases behind (bugfixes/new pickers, no known breaking changes)
     dependencies = {
         "nvim-lua/plenary.nvim",
-        "nvim-telescope/telescope-fzf-native.nvim",
+        -- build = "make": without it lazy never compiles the native
+        -- sorter at all (confirmed live - no .so ever produced), so this
+        -- sat installed and completely inert; load_extension below is the
+        -- other half, telling telescope to actually use it once built.
+        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
         "nvim-tree/nvim-web-devicons",
     },
     config = function()
@@ -23,11 +27,17 @@ return {
                 },
             },
         })
+        pcall(telescope.load_extension, "fzf")
 
         -- keymaps
         keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Fuzzy find files in cwd" } )
         keymap.set("n", "<leader>fs", builtin.live_grep, { desc = "Find string in cwd" } )
         keymap.set("n", "<leader>fc", builtin.grep_string, { desc = "Find string under cursor in cwd" } )
         keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "List normal mode keymappings" })
+        -- Previously missing entirely despite being some of the most
+        -- commonly used pickers.
+        keymap.set("n", "<leader>fb", builtin.buffers, { desc = "List open buffers" })
+        keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Search help tags" })
+        keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "List recently opened files" })
     end
 }
